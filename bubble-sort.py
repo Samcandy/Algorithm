ea1ba09e292338d30881e49bf939d6f5274bb932
Samcandy <samcandy841204@gmail.com>
2017-07-26 20:37:38 +0800

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,5 +1,4 @@
-def sort(data):
-    
+def sort(data): 
     for i in xrange(len(data)-1):
         for j in xrange(len(data)-i-1):
             if data[j]>data[j+1]:
@@ -7,7 +6,9 @@ def sort(data):
                 data[j] = data[j+1]
                 data[j+1] = space
     return data
+
 string_input=raw_input()
 input_list=string_input.split()
 input_list=[int(x) for x in input_list]
+
 print sort(input_list)
