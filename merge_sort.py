34237c76af36ff5988a66c6444eab9af9397169d
Samcandy <samcandy841204@gmail.com>
2017-10-12 08:57:13 +0800

    merge_sort

---
+++ merge_sort.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    def merge(left, right):
+        merged,left,right = deque(),deque(left),deque(right)
+        while left and right:
+            #deque popleft is also O(1)
+            merged.append(left.popleft() if left[0] <= right[0] else right.popleft())
+        merged.extend(right if right else left)
+        return list(merged)
+
+    middle = int(len(lst) // 2)
+    left = merge_sort(lst[:middle])
+    right = merge_sort(lst[middle:])
+    return merge(left,right)
+a=[5,8,2,9,1,0,7]
+print(merge_sort(a))
